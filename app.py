81345d57c833058e2dac6dd15711a01e913376f5
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-02-13 16:49:08 +0100

    reading voice sample from stream

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ import subprocess
 import struct
 import wave
 
+import pyaudio
+
 from mlpy import Dtw
 from numpy import array
 
 config = {'commands_folder': 'commands'}
+
+def unpack_wav(data, nframes, nchannels):
+    unpacked = struct.unpack_from("{0}h".format(nframes * nchannels), data)
+    first_channel = [unpacked[i] for i in range(0, len(unpacked), nchannels)]
+    out = array(list(first_channel))
+    return out
 
 class Command:
 
@@ -25,9 +33,7 @@ class Command:
         voice = wave.open(self.voice_path, 'rb')
         (nchannels, sampwidth, framerate, nframes, comptype, compname) = voice.getparams()
         frames = voice.readframes(nframes * nchannels)
-        unpacked = struct.unpack_from("{0}h".format(nframes * nchannels), frames)
-        first_channel = [unpacked[i] for i in range(0, len(unpacked), nchannels)]
-        out = array(list(first_channel))
+        out = unpack_wav(frames, nframes, nchannels)
         return out
 
 class VoiceCmd:
@@ -47,6 +53,22 @@ class VoiceCmd:
         for command in self.commands.itervalues():
             dist = self.dtw.compute(x, command.get_voice_array())
             print dist
+        print self.read_voice()
+
+    def read_voice(self):
+        rate, fpb, seconds, channels = 44100, 1024, 5, 1
+        read = []
+        p = pyaudio.PyAudio()
+        stream = p.open(format=pyaudio.paInt16, channels=channels, rate=rate, input=True, frames_per_buffer=fpb)
+        chunks_needed = rate / fpb * seconds
+        for i in xrange(chunks_needed):
+            data = stream.read(fpb)
+            read.append(data)
+        stream.close()
+        p.terminate()
+        data = ''.join(read)
+        out = unpack_wav(data, chunks_needed * fpb, channels)
+        return out
 
 
 if __name__ == '__main__':
